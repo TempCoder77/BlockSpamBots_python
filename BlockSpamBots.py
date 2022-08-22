75a042aa00cd0282fdb5bb2328fdc562579b1683
TempCoder77 <tempcoder77@gmail.com>
2022-08-22 19:53:17 +0200

    Uppdated code with init and self

--- BlockSpamBots.py
+++ BlockSpamBots.py
@@ -3,42 +3,51 @@ import os.path
 import csv
 
 
+from matplotlib.pyplot import text
+
+
 class BlockSpamBots:
 
-    def char_check(name, email, s):
-        ### Initiate lists with checker values for the loops to controll ###
-        name = name
-        email = email
-        alpa = "abcdefghijklmnopqrstuvwxyåäö1234567890 @!&()?+"
-        alpa_upper = alpa[0:-18].upper()
-       
-        checker =  list(alpa) + list(alpa_upper)
+    def __init__ (self, name, email, text):
+        self.name = name.lower()
+        self.email = email.lower()
+        self.text = text.lower()
         
+    # this is only usefull if we are doing something with it else we coud just call self.name (run_check.name)
+    def get_name(self):
+        return self.name.title()
+    
+    # this is only usefull if we are doing something with it else we coud just call self.email (run_check.email)
+    def get_email(self):
+        return self.email
+
+    def get_text(self):
+        ### Initiate lists with checker values for the loops to controll ###
+        ascii_chars = "abcdefghijklmnopqrstuvwxyåäö1234567890 @!&()?+"        
         bad_word = ["sex", "nude", "href", "xxx", "porn"]
-        #print(alpa_upper)
+        
         ### Loop and message list for chars to check chars return True for chars we don't understand or forbidden chars
-        bad_message = list(s)
+        bad_message = list(self.text)
         found_chars = []
         for ele in bad_message:
-            if ele not in checker:
-                found_chars.append(ele)
-        
+            if ele not in ascii_chars:
+                found_chars.append(ele)        
 
         ### Loop and message list for words and return the bad word to display for the user
-        bad_message = list(s.split(" "))
+        bad_message = list(self.text.split(" "))
         found_words = []
         for words in bad_word:
             if words in bad_message:
                 found_words.append(words)
         # if all is good return True for no error found in the checkers or return word found in words
         if found_chars or found_words:
-            BlockSpamBots.write_spamfile(name, email, s)
+            BlockSpamBots.write_spamfile(self)
             return found_chars + found_words
         else:
             return True
-
-    def write_spamfile(name, email, message):
-        rows = [name, email, message]
+    # This will save spam and a posible way to make it more efective 
+    def write_spamfile(self):
+        rows = [self.name, self.email, self.text]
         spam_file = Path("/home/mikael/Development/Python/BlockSpamBots/spamfile/spamtext.csv")
         if os.stat(spam_file).st_size == 0  and spam_file.is_file():
             
@@ -57,4 +66,5 @@ class BlockSpamBots:
 
 
 
-print(BlockSpamBots.char_check("Micke", "micke@micke.se", "Hello Whats up in the morning sex %%%===")) #tester case
+run_check = BlockSpamBots("Micke bot svensson", "micke@micke.se", "Hello Whats up in the morning se =% % % %") #tester case
+print(f"{run_check.get_name()} {run_check.get_text()}")
